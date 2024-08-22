49f924500800ed2172c9782c8c677c46c1a61c84
bithighlander <bithighlander@gmail.com>
2024-08-22 16:09:14 -0500

    EOD

--- chrome-extension/lib/background/chains/ethereumHandler.ts
+++ chrome-extension/lib/background/chains/ethereumHandler.ts
@@ -1,6 +1,49 @@
-import { JsonRpcProvider } from 'ethers';
+/*
+    Ethereum Provider
 
+ */
+
+import { JsonRpcProvider } from 'ethers';
+import { requestStorage, approvalStorage, assetContextStorage } from '@chrome-extension-boilerplate/storage';
+import axios from 'axios';
+// import { signMessage, signTransaction, signTypedData, broadcastTransaction, sendTransaction } from './sign';
+import { v4 as uuidv4 } from 'uuid';
 import { EIP155_CHAINS } from '../chains';
+
+const TAG = ' | ethereumHandler | ';
+const DOMAIN_WHITE_LIST = [];
+
+const CURRENT_PROVIDER: any = {
+  chainId: '0x1',
+  caip: 'eip155:1/slip44:60',
+  blockExplorerUrls: ['https://etherscan.io'],
+  name: 'Ethereum',
+  providerUrl: 'https://eth.llamarpc.com',
+  provider: new JsonRpcProvider('https://eth.llamarpc.com'),
+  fallbacks: [],
+};
+
+interface ChainInfo {
+  chainId: string;
+  name: string;
+  logo: string;
+  rgb: string;
+  rpc: string;
+  namespace: string;
+  caip: string;
+}
+
+interface Eip155Chains {
+  [key: string]: ChainInfo;
+}
+
+type Event = {
+  id: string;
+  type: string;
+  request: any;
+  status: 'request' | 'approval' | 'completed';
+  timestamp: string;
+};
 
 interface ProviderRpcError extends Error {
   code: number;
@@ -12,6 +55,72 @@ export const createProviderRpcError = (code: number, message: string, data?: unk
   error.code = code;
   if (data) error.data = data;
   return error;
+};
+
+let isPopupOpen = false; // Flag to track popup state
+
+const openPopup = function () {
+  const tag = TAG + ' | openPopup | ';
+  try {
+    console.log(tag, 'Opening popup');
+    chrome.windows.create(
+      {
+        url: chrome.runtime.getURL('popup/index.html'), // Adjust the URL to your popup file
+        type: 'popup',
+        width: 400,
+        height: 600,
+      },
+      window => {
+        if (chrome.runtime.lastError) {
+          console.error('Error creating popup:', chrome.runtime.lastError);
+          isPopupOpen = false;
+        } else {
+          console.log('Popup window created:', window);
+
+          // Continuously focus the popup to simulate "always on top"
+          // const intervalId = setInterval(() => {
+          //   chrome.windows.update(window.id, { focused: true }, updatedWindow => {
+          //     if (chrome.runtime.lastError) {
+          //       console.error('Error focusing popup:', chrome.runtime.lastError);
+          //       clearInterval(intervalId); // Stop the interval if there is an error
+          //     } else {
+          //       console.log('Popup window focused:', updatedWindow);
+          //     }
+          //   });
+          // }, 600); // Adjust the interval as needed
+          //
+          // // Optionally, clear the interval when the popup is closed
+          // chrome.windows.onRemoved.addListener(windowId => {
+          //   if (windowId === window.id) {
+          //     clearInterval(intervalId);
+          //     console.log('Popup window closed, interval cleared');
+          //   }
+          // });
+        }
+      },
+    );
+  } catch (e) {
+    console.error(tag, e);
+  }
+};
+
+const requireUnlock = async function () {
+  const tag = TAG + ' | requireUnlock | ';
+  try {
+    console.log(tag, 'requireUnlock for domain');
+    openPopup();
+  } catch (e) {
+    console.error(e);
+    isPopupOpen = false;
+  }
+};
+
+const convertHexToDecimalChainId = (hexChainId: string): number => {
+  return parseInt(hexChainId, 16);
+};
+
+const sanitizeChainId = (chainId: string): string => {
+  return chainId.replace(/^0x0x/, '0x');
 };
 
 export const handleEthereumRequest = async (
@@ -34,7 +143,7 @@ export const handleEthereumRequest = async (
     case 'net_version': {
       const netVersion = CURRENT_PROVIDER.chainId.toString();
       console.log(tag, 'Returning net_version:', netVersion);
-      return parseInt(netVersion, 16).toString();
+      return convertHexToDecimalChainId(netVersion).toString();
     }
     case 'eth_getBlockByNumber': {
       const blockByNumber = await CURRENT_PROVIDER.provider.getBlock(params[0]);
@@ -65,8 +174,13 @@ export const handleEthereumRequest = async (
       console.log(tag, 'Calling eth_call with:', params);
       const [callParams, blockTag, stateOverride] = params;
       console.log(tag, 'CURRENT_PROVIDER:', CURRENT_PROVIDER);
+      // Ensure callParams and blockTag are properly passed to the provider.call method
       const callResult = await CURRENT_PROVIDER.provider.call(callParams, blockTag, stateOverride);
       console.log(tag, 'callResult:', callResult);
+      const callResultString = JSON.stringify(callResult);
+      const callResultLength = callResultString.length;
+      console.log(tag, '(STEP 1) callResultLength:', callResultLength);
+
       return callResult;
     }
     case 'eth_estimateGas': {
@@ -83,21 +197,44 @@ export const handleEthereumRequest = async (
     case 'wallet_switchEthereumChain': {
       console.log(tag, 'Calling wallet_switchEthereumChain with:', params);
       if (!params || !params[0] || !params[0].chainId) throw new Error('Invalid chainId (Required)');
-      let chainId = 'eip155:' + parseInt(params[0].chainId, 16);
+      let chainId = 'eip155:' + convertHexToDecimalChainId(params[0].chainId);
+      chainId = sanitizeChainId(chainId);
       console.log(tag, 'Calling wallet_switchEthereumChain chainId:', chainId);
       if (params && params[0] && params[0].rpcUrls && params[0].rpcUrls[0]) {
         console.log(tag, 'Given Params for custom chain addition!');
-
+        /*
+            Example from thechainlist.org:
+            {
+              "blockExplorerUrls": ["https://bscscan.com"],
+              "chainId": "0x38",
+              "chainName": "BNB Chain LlamaNodes",
+              "nativeCurrency": {
+                "name": "BNB Chain Native Token",
+                "symbol": "BNB",
+                "decimals": 18
+              },
+              "rpcUrls": [
+                "https://binance.llamarpc.com",
+              ]
+            }
+        */
+        //blockExplorerUrls
         CURRENT_PROVIDER.blockExplorerUrls = params[0].blockExplorerUrls;
-        CURRENT_PROVIDER.chainId = params[0].chainId;
+        CURRENT_PROVIDER.chainId = sanitizeChainId(params[0].chainId);
         CURRENT_PROVIDER.caip = `eip155:${parseInt(params[0].chainId, 16)}/slip44:60`;
         CURRENT_PROVIDER.name = params[0].chainName;
         CURRENT_PROVIDER.nativeCurrency = params[0].nativeCurrency;
         CURRENT_PROVIDER.providerUrl = params[0].rpcUrls[0];
         CURRENT_PROVIDER.provider = new JsonRpcProvider(params[0].rpcUrls[0]);
+
+        //TODO check if loaded locally
+        //if new send to pioneer api
       } else {
         console.log(tag, 'chain switch (must be a known chain)');
-        const chainIdToFind = params[0].chainId;
+        /*
+            Assuming ONLY chainId is provided
+        */
+        const chainIdToFind = sanitizeChainId(params[0].chainId);
         console.log(tag, 'chainIdToFind:', chainIdToFind);
         let chainFound = false;
 
@@ -117,7 +254,27 @@ export const handleEthereumRequest = async (
           throw new Error(`Chain with chainId ${chainIdToFind} not found.`);
         }
       }
-
+      // const chain = EIP155_CHAINS[chainId];
+      //get market info by caip
+      if (CURRENT_PROVIDER.caip) {
+        // const marketInfo = await axios.get(`https://pioneers.dev/api/v1/assetByCaip/${CURRENT_PROVIDER.caip}`);
+        // console.log(tag, 'Market info:', marketInfo.data);
+        // CURRENT_PROVIDER.marketInfo = marketInfo.data;
+      }
+      //push to local storage
+      chrome.runtime.sendMessage({ type: 'PROVIDER_CHANGED', provider: CURRENT_PROVIDER });
+      assetContextStorage.updateContext(CURRENT_PROVIDER);
+      // if (chain) {
+      //   console.log('Found Chain in EIP155_CHAINS: ', chain);
+      //   CURRENT_PROVIDER.chainId = chain.chainId;
+      //   CURRENT_PROVIDER.name = chain.name;
+      //   CURRENT_PROVIDER.provider = new JsonRpcProvider(chain.rpc);
+      //   console.log(tag, `${method} switched to chain:`, chain.name);
+      //   return true;
+      // } else {
+      //   console.log('Not Found Chain: ', chainId);
+      //   throw createProviderRpcError(4902, 'Chain not found');
+      // }
       return true;
     }
     case 'wallet_getSnaps': {
@@ -150,7 +307,7 @@ export const handleEthereumRequest = async (
     case 'eth_sendTransaction':
     case 'personal_sign':
     case 'eth_sign': {
-      await requireApproval(requestInfo, 'ethereum', method, params[0]);
+      await requireApproval(requestInfo, 'etherum', method, params[0]);
       console.log(tag, 'Returning approval response for method:', method);
       return true;
     }
